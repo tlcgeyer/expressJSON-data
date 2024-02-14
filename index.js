c6a4e11a09fa851c3f177d1a5a5ea5d143722c88
tlcgeyer <tamlincupido2402@gmail.com>
2024-02-14 09:40:49 +0200

    backend

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import axios from 'axios'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 //Express App
 const app = express()
@@ -10,12 +12,24 @@ const port = +process.env.PORT || 4000
 //Router
 const router = express.Router()
 
+
 //JSON URL
 const dataURL = "https://tlcgeyer.github.io/vue.portfolioData-tlg/data/"
 //Application Middleware
 app.use(
     router
 )
+
+//Application Middleware
+app.use(
+    express.json(),
+    express.urlencoded({
+        extended: true
+    }),
+    cors(),
+    router
+)
+
 
 // / => home 
 router.get('^/$|/ejd', (req, res) => {
@@ -25,11 +39,13 @@ router.get('^/$|/ejd', (req, res) => {
     })
 })
 
+
 //Fetch all education data(this is where we specify what content of the json file we want to retrieve)
 router.get('/education', async (req, res)=> {
     // let {education} = await (await fetch 
     //     (dataURL)).json()
     // AN example with AXIOS
+    try {
     let response = await axios.get(dataURL)
     //to access the data
     let {education} = await response.data
@@ -37,9 +53,16 @@ router.get('/education', async (req, res)=> {
             status: res.statusCode,
             education
         })
+    }catch(e) {
+        res.json({
+            status: res.statusCode,
+            msg: 'Please try again later'
+        })
+    }
 })
 //calling forth only one specific set of data via the index
 router.get('/education/:id', async (req, res) => {
+    try {
     let response = await fetch(dataURL)
     let {education} = await response.json()
     let params = +req.params.id
@@ -48,23 +71,44 @@ router.get('/education/:id', async (req, res) => {
         status: res.statusCode,
         education: education[index]
     })
+    }catch(e) {
+        res.json({
+            status: res.statusCode,
+            msg: 'Please try again later.'
+        })
+    }
 })
 
 //making use of POST(which adds in a new set of data)
 router.post('/addEducation', async (req, res)=> {
-        let response = await axios.post(
-        dataURL, {
-            id: 3,
-            year: new Date().getFullYear(),
-            info: "Front-End Development Libraries Certificate.",
-            img: ""
+    try {
+        // let response = await axios.post(
+        // dataURL, {
+        //     id: 3,
+        //     year: new Date().getFullYear(),
+        //     info: "Front-End Development Libraries Certificate.",
+        //     img: ""
+        // })
+        let dataRes = await 
+        axios.post(dataURL,req.body )
+        if(dataRes) {
+            console.log(resVal.data);
+            res.json({ //sending a res back to the user
+                status: res.statusCode,
+                msg: 'New record was added'
+            })
         }
-    )
-    let {education} = response.data;//accessing the data property of the response
-    res.json({education}); //sending the education data in the response
-    res.status(200).json({ success: true });
-    
-})
+    }catch(e) {
+        console.log(e.message);
+        // 
+        res.json({ 
+            status: res.statusCode,
+            msg: 'An error occurred when adding a new data'
+        })
+    }
+    // let {education} = response.data;//accessing the data property of the response
+    // res.json({education}); //sending the education data in the response
+    // res.status(200).json({ success: true });
 //making use of PATCH(updates the data(eg.spelling error))
 router.patch('/updateEducation', (req, res)=>{
 
